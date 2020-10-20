4dad0beb177a80de438ef0ae1bd1f1512e888e8d
Robert Handi Wijaya <roberthandiwijaya@gmail.com>
2020-10-21 01:02:51 +0700

    Adding Cupertino Picker to the Apps

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +8,31 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+//  List<DropdownMenuItem> getDropDownItems() {
+//    List<DropdownMenuItem<String>> dropdownItems = [];
+//    for (String currency in currenciesList) {
+//      var newItem = DropdownMenuItem(
+//        child: Text(currency),
+//        value: currency,
+//      );
+//      dropdownItems.add(newItem);
+//    }
+//    return dropdownItems;
+//  }
+
+  List<Text> getPickerItems(){
+    List<Text> pickerItems = [];
+    for(String currency in currenciesList){
+      pickerItems.add(Text(currency));};
+    return pickerItems;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getPickerItems();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -42,10 +67,27 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: CupertinoPicker(
+              backgroundColor: Colors.lightBlue,
+              itemExtent: 32.0,
+              onSelectedItemChanged: (selectedIndex) {
+                print(selectedIndex);
+              },
+              children: getPickerItems(),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+//DropdownButton<String>(
+//value: selectedCurrency,
+//items: getDropDownItems(),
+//onChanged: (value) {
+//setState(() {
+//selectedCurrency = value;
+//});
+//},
+//),
